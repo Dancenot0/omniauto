a681a8140064857a728df64d2d47065ee628f3d3
Dancenot0 <dancenot02@gmail.com>
2026-01-30 19:20:52 -0500

    Create auto_stub.pyi

---
+++ test_project/auto_stub.pyi
@@ -0,0 +1,18 @@
+# ARCHIVO GENERADO AUTOMÁTICAMENTE POR OMNIAUTO
+# NO EDITAR MANUALMENTE
+
+from typing import Any, Callable
+
+class _UIStub:
+    def saludar(nombre: str): ...
+
+class _DataStub:
+    def sumar(a: int, b: int): ...
+
+class OmniAutoStub:
+    ui: _UIStub
+    data: _DataStub
+    utils: Any
+    prueba: Any
+
+auto: OmniAutoStub
