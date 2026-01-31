b1055e423da071f43062f5315a9d0c9becfd040e
Dancenot0 <dancenot02@gmail.com>
2026-01-30 19:04:12 -0500

    Add files via upload

---
+++ auto_stub.pyi
@@ -0,0 +1,15 @@
+# ARCHIVO GENERADO AUTOMÁTICAMENTE POR OMNIAUTO
+# NO EDITAR MANUALMENTE
+
+from typing import Any, Callable
+
+class _UIStub:
+
+class _DataStub:
+
+class OmniAutoStub:
+    ui: _UIStub
+    data: _DataStub
+    utils: Any
+
+auto: OmniAutoStub
